34482691e7e3e43466a15fe2235c82c816b04b47
DARSHAN J PATEL <102922350+DJp119@users.noreply.github.com>
2022-09-25 14:24:16 +0530

    Create Longest Subarray With Maximum Bitwise AND.cpp
    https://leetcode.com/problems/longest-subarray-with-maximum-bitwise-and/discuss/2620657/Simple-bitwise-and(AND)-property-to-solve-this-problem-easily.

---
+++ leetcode/Weekly Contest 312/Longest Subarray With Maximum Bitwise AND.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int longestSubarray(vector<int>& nums) {
+        int mxm=0;       
+        for(int i=0;i<nums.size();i++)           //finding maximum value
+            mxm=max(mxm,nums[i]);
+        int cnt=0,i,ans=1;                           
+        for(i=0;i<nums.size();i++){             //finding continuous occurences of mxm number 
+            if(nums[i]==mxm){
+                cnt++;
+            }
+            else
+                cnt=0;
+            ans=max(ans,cnt);
+        }
+        return ans;
+    }
+};
+
+
+/* 
+TC: O(N)
+SC: O(1) (auxiliary space)
+where N is size of num array.
+*/
